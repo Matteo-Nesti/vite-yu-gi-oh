5b559a7015911d791d401b8264fd41d4c754ffea
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-07-03 17:38:07 +0200

    add cards

--- src/components/characters/CharacterCard.vue
+++ src/components/characters/CharacterCard.vue
@@ -1,6 +1,11 @@
 <template >
-    <div class="pokemonCard">
-        <div>{{ name }}</div>
+    <div class="pokemonCard" :class="color">
+        <div class="img-container">
+            <img :src="image" :alt="name" class="rounded-circle img-fluid">
+        </div>
+        <div>{{ number }}</div>
+        <h5>{{ name }}</h5>
+        <i>{{ type1 }} {{ type2 }}</i>
     </div>
 </template>
 
@@ -17,4 +22,33 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.pokemonCard {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 1rem 0;
+    padding: 1rem;
+    border-radius: 10px;
+}
+
+.img-container {}
+
+.Red {
+    background-color: #fddfdf;
+}
+
+.Green {
+    background-color: #defde0;
+}
+
+.Blue {
+    background-color: #def3fd;
+}
+
+.Black {
+    background-color: #747474;
+}
+</style>
--- src/components/characters/CharactersList.vue
+++ src/components/characters/CharactersList.vue
@@ -1,7 +1,9 @@
 <template>
     <section id="characters">
-        <div v-for="character in store.pokemonList">
-            <CharacterCard :name="character.name" :image="character.imageUrl" :type1="character.type1" :type2="character.type2" :color="character.color" :number="character.number" />
+        <div class="row row-cols-5">
+            <div v-for="character in store.pokemonList">
+                <CharacterCard :name="character.name" :image="character.imageUrl" :type1="character.type1" :type2="character.type2" :color="character.color" :number="character.number" />
+            </div>
         </div>
     </section>
 </template>
